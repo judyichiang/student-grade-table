f59f8b7a748457c1b2fe7f285c31d5cea07f154d
Judy Chiang <jiyc1119@gmail.com>
2020-02-07 14:57:42 -0800

    added click event to delete button

--- components/grade-table.js
+++ components/grade-table.js
@@ -25,8 +25,34 @@ class GradeTable {
     this.deleteGrade = deleteGrade;
   }
 
-  renderGradeRow(data, deleteGrade){
+  renderGradeRow(data, deleteGrade) {
+    console.log("renderGradeRow:", data);
 
+    this.tableElement.textContent = "";
+    for (var i = 0; i < data.length; i++) {
+      var tr = document.createElement("tr");
+      var td1 = document.createElement("td");
+      var td2 = document.createElement("td");
+      var td3 = document.createElement("td");
+      var td4 = document.createElement("td");
+      var deleteButton = document.createElement("button");
+      deleteButton.addEventListener("click", deleteGrade);
+
+      function deleteGrade() { }
+
+      td1.textContent = data[i].name;
+      tr.appendChild(td1);
+      td2.textContent = data[i].course;
+      tr.appendChild(td2);
+      td3.textContent = data[i].grade;
+      tr.appendChild(td3);
+
+
+
+      td4.appendChild(deleteButton);
+      tr.appendChild(td4);
+      this.tableElement.appendChild(tr);
+    }
 
 
 
