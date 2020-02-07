4cacab6f59919446fb31091b713b5e139c2f8e62
Judy Chiang <jiyc1119@gmail.com>
2020-02-07 15:54:58 -0800

    update method

--- components/grade-table.js
+++ components/grade-table.js
@@ -6,18 +6,9 @@ class GradeTable {
   updateGrades(grades) {
     console.log("Update Grades:", grades);
     this.tableElement.textContent = "";
+
     for (var i = 0; i < grades.length; i++) {
-      var tr = document.createElement("tr");
-      var td1 = document.createElement("td");
-      var td2 = document.createElement("td");
-      var td3 = document.createElement("td");
-      td1.textContent = grades[i].name;
-      tr.appendChild(td1);
-      td2.textContent = grades[i].course;
-      tr.appendChild(td2);
-      td3.textContent = grades[i].grade;
-      tr.appendChild(td3);
-      this.tableElement.appendChild(tr);
+      this.renderGradeRow(grades[i], this.deleteGrade);
     }
   }
 
@@ -36,26 +27,20 @@ class GradeTable {
       var td3 = document.createElement("td");
       var td4 = document.createElement("td");
       var deleteButton = document.createElement("button");
-      deleteButton.addEventListener("click", deleteGrade);
-
-      function deleteGrade() { }
+      deleteButton.addEventListener("click", function () {
+        deleteGrade(data.id);
+      });
 
       td1.textContent = data[i].name;
-      tr.appendChild(td1);
       td2.textContent = data[i].course;
-      tr.appendChild(td2);
       td3.textContent = data[i].grade;
-      tr.appendChild(td3);
-
-
-
       td4.appendChild(deleteButton);
+
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      tr.appendChild(td3);
       tr.appendChild(td4);
       this.tableElement.appendChild(tr);
     }
-
-
-
   }
-
 }
