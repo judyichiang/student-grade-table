3b45c84d9da483ad2400d817c56fba69e13f38b7
Judy Chiang <jiyc1119@gmail.com>
2020-02-07 16:22:10 -0800

    this.gradeTable.onDeleteClick(this.deleteGrade)

--- components/apps.js
+++ components/apps.js
@@ -47,6 +47,8 @@ class App {
   start() {
     this.getGrades();
     this.gradeForm.onSubmit(this.createGrade);
+    this.gradeTable.onDeleteClick(this.deleteGrade);
+
   }
 
   createGrade(name, course, grade) {
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var header = document.querySelector("header");
 
 var tbody = document.querySelector("tbody");
+var hidden = document.querySelector("p");
 
 var form = document.querySelector("form")
 
-var gradeTable = new GradeTable(tbody);
+var gradeTable = new GradeTable(tbody, hidden);
 var pageHeader = new PageHeader(header);
 
 var gradeForm = new GradeForm(form);
